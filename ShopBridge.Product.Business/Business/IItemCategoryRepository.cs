1df758fe32d77fab80817489e25716d96de235fc
mayurpawar071187 <72074811+mayurpawar071187@users.noreply.github.com>
2021-12-03 01:34:43 +0530

    Add files via upload

---
+++ ShopBridge.Product.Business/Business/IItemCategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShopBridge.Product.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShopBridge.Product.Business
+{
+    public interface IItemCategoryRepository
+    {
+        Task<string> Add(ItemCategory item);
+        Task<bool> update(string id, ItemCategory item);
+        Task<bool> delete(string id);
+        Task<ItemCategory> get(string id);
+        Task<List<ItemCategory>> getlist();
+    }
+}
---
+++ ShopBridge.Product.Business/Business/IItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShopBridge.Product.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShopBridge.Product.Business
+{
+    public interface IItemRepository
+    {
+        Task<string> Add(Item item);
+        Task<bool> update(string id,Item item);
+        Task<bool> delete(string id);
+        Task<Item> get(string id);
+        Task<List<Item>> getlist(ItemSearch itemsearch);
+    }
+}
